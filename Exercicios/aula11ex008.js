f651d1f52268cb2f4e5ba2d8e230d405d8ce2511
EnzoSilva13 <188266511+EnzoSilva13@users.noreply.github.com>
2025-01-02 17:04:54 -0300

    atualizaçoes

---
+++ Exercicios/aula11ex008.js
@@ -0,0 +1,7 @@
+var vel = 79
+console.log(`a velocidade do seu carro é ${vel} quilometros por hora`)
+if(vel >=80){
+    console.log('voce ultrapassou a velocidade permitida. MULTADO')
+}
+console.log('dirija sempre usando cinto de segurança')
+//condinçao simples
---
+++ Exercicios/aula11ex009.js
@@ -0,0 +1,6 @@
+var pais= 0
+if(pais == 'Brasil'){
+    console.log('é Brasileiro')
+} else {
+    console.log('estrangeiro')
+}
