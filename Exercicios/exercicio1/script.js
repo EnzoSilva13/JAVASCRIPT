2d6cdb0a2a05ddd9bb873cad13bace63bb878a15
EnzoSilva13 <188266511+EnzoSilva13@users.noreply.github.com>
2025-01-08 17:10:53 -0300

    Update script.js

--- Exercicios/exercicio1/script.js
+++ Exercicios/exercicio1/script.js
@@ -2,18 +2,21 @@ function carregar(){
     var msg= window.document.getElementById('msg')
     var img= window.document.getElementById('imagem')
     var data = new Date()
-    //var hora= data.getHours()
-    hora= 8
+    var hora= data.getHours()
+    
 
     msg.innerHTML = `Agora são ${hora} horas.`
     if(hora >=5 && hora< 12)/*bom dia*/{
         img.src = 'imagens/manha.png'
-        document.body.style.background = '#89a4c6'        
+        document.body.style.background = '#fef4d1'        
     }else if(hora >=12 && hora < 17)/*boa tarde*/{
         img.src = 'imagens/meiodia.png'
+        document.body.style.background = '#89a4c6'
     }else if(hora >=17 && hora < 18)/*Boa tarde-tardecer*/{
         img.src = 'imagens/tarde.png'
+        document.body.style.background = '#c25616'
     }else
         img.src = 'imagens/noite.png'
+        document.body.style.background = '#011842'
 
 }
