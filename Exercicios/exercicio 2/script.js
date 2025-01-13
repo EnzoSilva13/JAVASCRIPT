da80027463770ef6e8f21c18fa6810bda9329759
EnzoSilva13 <188266511+EnzoSilva13@users.noreply.github.com>
2025-01-13 16:25:12 -0300

    atualizaçoes

--- Exercicios/exercicio 2/script.js
+++ Exercicios/exercicio 2/script.js
@@ -1,11 +1,17 @@
-function carregar(){
-var msg= window.document.getElementById('msg')
-var foto= window.document.getElementById('foto')
+function verificar(){
+    var data = new Date()
+    var anoatual = data.getFullYear()
+    var anonas = document.getElementById('txtano')
+    var anonas = Number(anonas.value)
+    var msg = document.querySelector('msg')
+    
 
-var data = new Date()
-var ano = window.document.getElementById('txt')
-var ano = Number(ano.value)
+    if(anonas.value.length == 0 || Number(anonas.value) > anoatual){
+        window.alert('[ERRO] verifique seus dados e tente novamente')
+    } else if {
+        
+        idade = anoatual - anonas
+        msg.innerHTML=`sua idade é ${idade}` 
 
-
-msg.innerHTML= `sua idade é ${ano} anos`
+    }
 }
