12211c440b6807df10198247a9ab11e8792d122f
EnzoSilva13 <188266511+EnzoSilva13@users.noreply.github.com>
2025-01-29 21:54:25 -0300

    atualizaçao

--- Exercicios/exercicio de casa 3/script.js
+++ Exercicios/exercicio de casa 3/script.js
@@ -1,5 +1,25 @@
 function calcular(){
-    var res = document.getElementsByClassName('numbers')
-    alert('res')
+    var num1= document.getElementById('num1')
+        var n1= Number(num1.value)
+    var num2= document.getElementById('num2')
+        var n2= Number(num2.value)
+    var num3= document.getElementById('num3')
+        var n3= Number(num3.value)
+    var num4= document.getElementById('num4')
+        var n4= Number(num4.value)
+    var res = document.getElementById('res')
+  
+  if(n1 <= 0 || n2 <= 0 || n3 <= 0 | n4 <= 0){
+        alert('erro')
+  }else {
+    let c = (n1+n2+n3+n4)/4
+    if(c >= 7){
+        res.innerHTML= `Parabens sua media é ${c} , voce passou!`
+    } else {
+        res.innerHTML= `sua media é ${c}, reprovado!`
+    }
+    }
 
+    
+  
 }
