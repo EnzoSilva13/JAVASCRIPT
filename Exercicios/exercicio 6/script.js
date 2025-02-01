779bc5532bfd93e9826e6fd484af00497e4d4f93
EnzoSilva13 <188266511+EnzoSilva13@users.noreply.github.com>
2025-02-01 13:47:27 -0300

    criaçao do exericio 6

--- Exercicios/exercicio 6/script.js
+++ Exercicios/exercicio 6/script.js
@@ -1,1 +1,30 @@
-var txtnum = 
+let num= document.getElementById('txtn1')
+let tab= document.querySelector('select#tab')
+let res= document.querySelector('div#res')
+let valores = []
+
+function essenumero(n){
+    if(Number(n) >= 1 && Number(n) <=100){
+        return true
+    }else {
+        return false
+    }
+
+}
+function inTab(n, t) {
+    if (t.indexof(Number(num)) != -1){
+        return true
+    }else {
+        return false
+    }
+
+}
+
+function adicionar(){
+    if(essenumero(num.value) && inTab(num.value, valores)){
+        alert('tudo ok')
+
+    }else {
+        alert('valor nao encontrado ou ja na lista')
+    }
+}
