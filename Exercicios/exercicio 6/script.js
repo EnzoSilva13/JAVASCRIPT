e9f63d201556c9a659377e3c820eb743f493e0a3
EnzoSilva13 <188266511+EnzoSilva13@users.noreply.github.com>
2025-02-03 19:14:33 -0300

    o

--- Exercicios/exercicio 6/script.js
+++ Exercicios/exercicio 6/script.js
@@ -12,7 +12,7 @@ function essenumero(n){
 
 }
 function inTab(n, t) {
-    if (t.indexof(Number(num)) != -1){
+    if (t.indexOf(Number(n)) != -1){
         return true
     }else {
         return false
@@ -21,10 +21,43 @@ function inTab(n, t) {
 }
 
 function adicionar(){
-    if(essenumero(num.value) && inTab(num.value, valores)){
-        alert('tudo ok')
+    if(essenumero(num.value) && !inTab(num.value, valores)){
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `o valor ${num.value} foi adicionado.`
+        tab.appendChild(item)
 
     }else {
         alert('valor nao encontrado ou ja na lista')
+    }
+    num.value =''
+    num.focus()
+}
+function finalizar(){
+    if(valores.length == 0){
+        alert('adicione um valor para começar.')
+    } else {
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let media = 0
+        let soma = 0
+        for(let pos in valores){
+            soma += valores[pos]
+            if(valores[pos] > maior){
+                maior = valores[pos]
+            }
+            if(valores[pos] < menor){
+                menor = valores[pos]
+            }    
+            
+        }
+        media = soma/tot
+        res.innerHTML = ''
+        res.innerHTML += `<p> ao todo temos ${tot} numeros cadastrados`
+        res.innerHTML += `<p> o maior valor informado foi ${maior}`
+        res.innerHTML += `<p> o menor valor informado foi ${menor}</p>`
+        res.innerHTML += `<p> a soma de todos os valores é ${soma}`
+        res.innerHTML += `<p> a media dos valores é ${media}`
     }
 }
